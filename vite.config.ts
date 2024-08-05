3cdd08a02081891d3446a1b8eccc8eabd7fa31d8
rexvel <rexvelele@gmail.com>
2024-08-05 17:11:02 +0300

    chore: configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
   plugins: [react()],
-})
+});
