ee335b94f552ddec368e6ce8ae574014942cab95
rexvel <rexvelele@gmail.com>
2024-08-05 17:21:38 +0300

    fix: type error

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vitest/config';
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    include: ['**/?(*.)test.ts?(x)'],
   },
-  plugins: [react()],
 });
