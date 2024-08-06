1dc2ec2ee6db38ba52913b8d8dfc57db81a5b682
rexvel <rexvelele@gmail.com>
2024-08-06 17:44:37 +0300

    test: move to separate folder

---
+++ test/App.test.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import { describe, it } from 'vitest';
+import App from '../src/App';
+
+describe('App component', () => {
+  it('renders Vite and React logos', () => {
+    render(<App />);
+  });
+});
