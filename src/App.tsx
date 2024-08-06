af445dbe007800f0f869ffc3c5f90b72fad47037
rexvel <rexvelele@gmail.com>
2024-08-06 12:02:18 +0300

    feat: add layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
+import { Layout } from './components/Layout';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Layout>
+      <div className="flex flex-col md:flex-row gap-4">
+        <main className="w-full">
+          <h1 className="text-4xl font-bold">Welcome to Vite!</h1>
+        </main>
       </div>
-      <h1 className="text-6xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    </Layout>
   );
 }
 
