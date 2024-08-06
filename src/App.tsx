a78445f96cbeaf330f7f6eebb9bc573b02df2e4c
rexvel <rexvelele@gmail.com>
2024-08-06 17:44:20 +0300

    feat: add pdf viewer (POC)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,50 @@
-import { ConversionForm } from './components/ConversionForm';
+import React, { ReactNode, useState } from 'react';
 import { Layout } from './components/Layout';
-import './App.css';
+import { convertToPdf } from './api';
+import { Button, Textarea } from './components';
+import PdfView from './components/PDFViewer';
 
-function App() {
-  const { Container, TextArea, SubmitButton } = ConversionForm;
+import './App.css';
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+
+interface OnlyProps {
+  when: unknown;
+  children: ReactNode;
+}
+
+const Only: React.FC<OnlyProps> = ({ when, children }) => {
+  return when ? <>{children}</> : null;
+};
+
+const App: React.FC = () => {
+  const [text, setText] = useState<string>('');
+  const [pdfUrl, setPdfUrl] = useState<string | null>(null);
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setText(event.target.value);
+  };
+
+  const handleConvert = async (): Promise<void> => {
+    try {
+      const url = await convertToPdf(text);
+      setPdfUrl(url);
+    } catch (error) {
+      console.error('Error converting to PDF:', error);
+    }
+  };
 
   return (
     <Layout>
-      <Container>
-        <TextArea />
-        <SubmitButton>Convert to PDF</SubmitButton>
-      </Container>
+      <main className="flex flex-col items-center justify-center space-y-4 max-w-md mx-auto">
+        <Textarea value={text} onChange={handleTextChange} />
+        <Button onClick={handleConvert}>Convert file</Button>
+        <Only when={pdfUrl}>
+          <PdfView file={`data:application/pdf;base64,${pdfUrl}`} />
+        </Only>
+      </main>
     </Layout>
   );
-}
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { pdfjs } from 'react-pdf';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+pdfjs.GlobalWorkerOptions.workerSrc = new URL('pdfjs-dist/build/pdf.worker.min.mjs', import.meta.url).toString();
+
+createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
