a173523b3e4becd310852da82aea716512f2031f
rexvel <rexvelele@gmail.com>
2024-08-06 17:38:36 +0300

    chore: update configs

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,30 @@
 
 import path from 'path';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { defineConfig, normalizePath } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'));
+const standardFontsDir = normalizePath(
+  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'standard_fonts'),
+);
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: cMapsDir, dest: '' },
+        { src: standardFontsDir, dest: '' },
+      ],
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
+    setupFiles: './test/vitest.setup.ts',
     include: ['**/?(*.)test.ts?(x)'],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
   },
 });
