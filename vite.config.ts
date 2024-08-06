48c6821535d90546abee71a29a4655927cf81850
rexvel <rexvelele@gmail.com>
2024-08-06 10:31:16 +0300

    chore: update configs;

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 
+import path from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
@@ -10,5 +11,10 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     include: ['**/?(*.)test.ts?(x)'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
